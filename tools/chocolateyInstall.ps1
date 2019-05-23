0d47ea1090034dbffca18214ec0edd6d79361526
Xavier Plantefeve <xavier@noreply.plantefeve.net>
2019-05-23 20:34:31 +0200

    New version of the package
    No bumping of Grisbi version, but the package has been updated to be
    more "modern" and include new fancy NuSpec and chocolatey features.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,23 @@
 $packageName = 'Grisbi'
-$installerType = 'exe'
-$url = 'https://sourceforge.net/projects/grisbi/files/grisbi%20stable/1.2.x/1.2.2/Grisbi-64bit-1.2.2-setup.exe/download'
-$silentArgs = '/S'
-$validExitCodes = @(0)
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
+$Url = 'https://sourceforge.net/projects/grisbi/files/grisbi%20stable/1.2.x/1.2.2/Grisbi-32bit-1.2.2-setup.exe/download'
+$Url64 = 'https://sourceforge.net/projects/grisbi/files/grisbi%20stable/1.2.x/1.2.2/Grisbi-64bit-1.2.2-setup.exe/download'
+$Checksum = '8084A63F65FF68EA6DAF5267BECB23DD32E74235761AB97023C7ADE13F4F8D7B'
+$Checksum64 = '31EAACD03EA911523E7B81A1873C653C5DEBA770AA1E0DCC4F3B9C2264CFE5E6'
+
+$packageArgs = @{
+    packageName    = $packageName
+    fileType       = 'exe'
+    url            = $url
+    url64bit       = $url64
+    silentArgs     = '/S'
+    validExitCodes = @(0)
+    softwareName   = 'Grisbi*'
+    checksum       = $Checksum
+    checksumType   = 'sha256'
+    checksum64     = $Checksum64
+    checksumType64 = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
