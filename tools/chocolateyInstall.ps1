64772a4bb7758b4489e809a97836d70ea9713328
Xavier Plantefeve <xavier@noreply.plantefeve.net>
2021-01-25 16:18:30 +0100

    Updated for Grisbi 2.0.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $packageName = 'Grisbi'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
-$Url = 'https://sourceforge.net/projects/grisbi/files/grisbi%20stable/1.2.x/1.2.2/Grisbi-32bit-1.2.2-setup.exe/download'
-$Url64 = 'https://sourceforge.net/projects/grisbi/files/grisbi%20stable/1.2.x/1.2.2/Grisbi-64bit-1.2.2-setup.exe/download'
-$Checksum = '8084A63F65FF68EA6DAF5267BECB23DD32E74235761AB97023C7ADE13F4F8D7B'
-$Checksum64 = '31EAACD03EA911523E7B81A1873C653C5DEBA770AA1E0DCC4F3B9C2264CFE5E6'
+$Url = 'https://sourceforge.net/projects/grisbi/files/grisbi%20stable/2.0.x/2.0.0/Grisbi-32bit-2.0.0-01-setup.exe/download'
+$Url64 = 'https://sourceforge.net/projects/grisbi/files/grisbi%20stable/2.0.x/2.0.0/Grisbi-64bit-2.0.0-01-setup.exe/download'
+$Checksum = '717E2675302D315898D3D165DA9D090421139B8508BBE1495ECD16876D065624'
+$Checksum64 = '9856BD93BBA308ADCEC327EF1186B85E16E3FD5B9ADC657C2352D15E28D93AB6'
 
 $packageArgs = @{
     packageName    = $packageName
